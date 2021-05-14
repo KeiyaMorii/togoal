71943d0b46e59eee5fcdc090c32bd1e9464a0f91
KeiyaMorii <kei8.m.0123@gmail.com>
2021-05-14 23:32:06 +0900

    Top screen html

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <title>To Goal</title>
+    <style>
+    h1 { font-size: 15pt; text-align: right; color: black; margin: 5px 20px;}
+    table { table-layout: fixed; width: 1000; height: 215; margin-left: auto; margin-right: auto;}
+    </style>
+</head>
+<body>
+    <h1>会員登録</h1>
+    <h1>ログイン</h1>
+    <table border="1">
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+    </table>
+    <table border="1">
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+    </table>
+    <table border="1">
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+        <tr>
+        <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        </tr>
+    </table>
+</body>
+</html>
