7d3abf0293b156e99f5679bf7cfa59b6bdd0e86d
KeiyaMorii <kei8.m.0123@gmail.com>
2021-05-19 23:18:19 +0900

    Home screen change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HelloController extends Controller
+class HomeController extends Controller
 {
     public function index()
     {
-        return view('hello.user');
+        return view('home.index');
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,14 +2,17 @@
 <head>
     <title>To Goal</title>
     <style>
-    h1 { font-size: 15pt; text-align: right; color: black; margin: 5px 20px;}
-    table { table-layout: fixed; width: 1100; height: 300; margin-left: auto; margin-right: auto;}
+    h1 { font-family: serif; font-size: 30px; color: black; float: right; margin-top: 200; margin-right: 400; }
+    h2 { font-family: serif; font-size: 30px; color: black; float: right; margin-top: 350; margin-right: -123; }
+    table { table-layout: fixed; width: 620; height: 215; margin-left: 10; }
+    .mandara {margin-top: 50px;}
     </style>
 </head>
 <body>
     <h1>会員登録</h1>
-    <h1>ログイン</h1>
-    <table border="1">
+    <h2>ログイン</h2>
+    <div class="mandara">
+        <table border="1">
         <tr>
         <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
         </tr>
@@ -42,5 +45,6 @@
         <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
         </tr>
     </table>
+    </div>
 </body>
 </html>
